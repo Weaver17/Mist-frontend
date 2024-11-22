aaee7b8c7689286e4a0a71cfda4b59aac281e6c8
Andrew <ajweaver18@gmail.com>
2024-11-21 22:44:17 -0500

    base change

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Mist-frontend/",
+  base: "",
   server: {
     port: 3003,
   },
   build: {
     outDir: "dist",
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
   },
 });
