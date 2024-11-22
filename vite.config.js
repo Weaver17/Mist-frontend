515430127fbaccb236d3e4fb468e4d8c0e5f200d
Andrew <ajweaver18@gmail.com>
2024-11-21 21:27:19 -0500

    base fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "Mist-frontend",
+  base: "/Mist-frontend",
   server: {
     port: 3003,
   },
