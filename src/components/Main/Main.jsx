8021e99f495642bb86f695fa91c34b381f653df3
Weaver17 <ajweaver18@gmail.com>
2025-09-03 09:34:19 -0400

    fix: main cleanup

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useEffect } from "react";
 
 import GameCard from "../GameCard/GameCard";
 import Preloader from "../Preloader/Preloader";
@@ -9,52 +9,56 @@ import { useGames } from "../../contexts/GameContext";
 import ToTopBtn from "../Buttons/ToTopBtn/ToTopBtn";
 
 const Main = ({
-  handleGameClick,
-  favoritedGames,
-  setFavoritedGames,
-  savedGames,
-  setSavedGames,
-  onToTopClick,
-  scrollPosition,
+    handleGameClick,
+    favoritedGames,
+    setFavoritedGames,
+    savedGames,
+    setSavedGames,
+    onToTopClick,
+    scrollPosition,
 }) => {
-  const { games, getNewestGames, isLoading, visibleCount } = useGames();
+    const { games, getNewestGames, isLoading, visibleCount } = useGames();
 
-  useEffect(() => {
-    getNewestGames();
-  }, []);
+    useEffect(() => {
+        getNewestGames();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-  return (
-    <section className="main">
-      {/* FEATURED GAME */}
+    return (
+        <section className="main">
+            {/* FEATURED GAME */}
 
-      <h2 className="main__card-list-title">Newest Releases:</h2>
+            <h2 className="main__card-list-title">Newest Releases:</h2>
 
-      {/* GAME CARD GRID */}
-      <ul className="main__card-list">
-        {isLoading ? (
-          <Preloader />
-        ) : (
-          games.slice(0, visibleCount).map((game) => {
-            return (
-              <GameCard
-                key={game.id}
-                game={game}
-                onGameClick={handleGameClick}
-                favoritedGames={favoritedGames}
-                setFavoritedGames={setFavoritedGames}
-                savedGames={savedGames}
-                setSavedGames={setSavedGames}
-              />
-            );
-          })
-        )}
-      </ul>
-      {!isLoading && visibleCount < games.length && (
-        <ShowMoreBtn type="button" classModifier="main" />
-      )}
-      <ToTopBtn onToTopClick={onToTopClick} scrollPosition={scrollPosition} />
-    </section>
-  );
+            {/* GAME CARD GRID */}
+            <ul className="main__card-list">
+                {isLoading ? (
+                    <Preloader />
+                ) : (
+                    games.slice(0, visibleCount).map((game) => {
+                        return (
+                            <GameCard
+                                key={game.id}
+                                game={game}
+                                onGameClick={handleGameClick}
+                                favoritedGames={favoritedGames}
+                                setFavoritedGames={setFavoritedGames}
+                                savedGames={savedGames}
+                                setSavedGames={setSavedGames}
+                            />
+                        );
+                    })
+                )}
+            </ul>
+            {!isLoading && visibleCount < games.length && (
+                <ShowMoreBtn type="button" classModifier="main" />
+            )}
+            <ToTopBtn
+                onToTopClick={onToTopClick}
+                scrollPosition={scrollPosition}
+            />
+        </section>
+    );
 };
 
 export default Main;
